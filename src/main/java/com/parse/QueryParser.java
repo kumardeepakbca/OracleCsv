f3c3c4d0f6611e4ac719995bdf267ad12d7628f1
Deepak Kumar <deepakk@tarangtech.local>
2017-06-01 18:11:24 +0530

    Issue fixes

--- src/main/java/com/parse/QueryParser.java
+++ src/main/java/com/parse/QueryParser.java
@@ -2,7 +2,7 @@ package com.parse;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -12,48 +12,34 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 public class QueryParser {
-
-	/*
-	 * public static void main(String[] args) throws IOException,
-	 * InterruptedException { String outputPath="";
-	 * 
-	 * try { loadData(outputPath,args[0],args[1]);
-	 * //loadData(outputPath,"C:\\data\\"
-	 * ,"C:\\properties\\database.properties");
-	 * 
-	 * } catch (Exception e) { System.out.println(e.getMessage());
-	 * e.printStackTrace();
-	 * 
-	 * }
-	 * 
-	 * }
-	 */
 
 	public static void main(String[] args) throws Exception {
 		String outputPath = "";
 
 		try {
-			loadData(outputPath, args[0], args[1]);
-			// loadData(outputPath,"C:\\data\\","C:\\properties\\database.properties");
+			loadData(outputPath, args[0]);
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 
 		}
-		
 
-		
 	}
 
-	private static Map getCSVHeaderData() {
-		Map csvData = new HashMap();
+	private static LinkedHashMap getCSVHeaderData() {
+		LinkedHashMap csvData = new LinkedHashMap();
 		csvData.put("0", "LOAN_NUM");
 		csvData.put("1", "WORKFLOW");
 		csvData.put("2", "RFP_DT");
@@ -92,65 +78,60 @@ public class QueryParser {
 		return csvData;
 	}
 
-	static void loadData(String outputPath, String csvLocation,
+	static void loadData(String outputPath,
 			String propertiesFilePath) {
-		System.out.println("csvLocation :- " + csvLocation);
 		System.out.println("propertiesFilePath :- " + propertiesFilePath);
 		ResultSet rs = null;
 		Properties properties = new Properties();
 		try {
-			// String csvLocation = args[1];
 			int year = Calendar.getInstance().get(Calendar.YEAR);
 			int month = Calendar.getInstance().get(Calendar.MONTH);
-			int day = Calendar.getInstance().get(Calendar.DATE);
-			String fileName = "110_Report_" + day + month + year + ".csv";
-			if (csvLocation == null || "".equals(csvLocation.trim())) {
-				csvLocation = "C:\\data\\" + fileName;
-			} else {
-				csvLocation = csvLocation + fileName;
+			String m = "";
+			if (month < 10) {
+				m = "0" + month;
 			}
-			System.out.println("csvLocation full path :- " + csvLocation);
+			int day = Calendar.getInstance().get(Calendar.DATE);
+			String d="";
+			if (day < 10) {
+				d = "0" + day;
+			}
+			String fileName = "MT559_110_Locked_Report_" + d + m + year
+					+ ".xlsx";
 			properties.load(new FileInputStream(propertiesFilePath));
+			properties.load(new FileInputStream(properties.getProperty("config.path")));
+			properties.load(new FileInputStream(properties.getProperty("sql.path")));
 
 			int tableCount = 0;
 			List colsData = new ArrayList();
 			String timePeriod = "";
 			Map csvData = new HashMap();
-			FileWriter writer = null;
-			File f = new File(csvLocation);
-			if (!f.exists()) {
-				List colsMapHeader = new ArrayList();
-				Map map = getCSVHeaderData();
-				Iterator iter = map.entrySet().iterator();
-
-				while (iter.hasNext()) {
-					Map.Entry mEntry = (Map.Entry) iter.next();
-					colsMapHeader.add(mEntry.getValue());
-				}
-				
-				
-				writer = new FileWriter(csvLocation, true);
-				CSVUtils.writeLine(writer, colsMapHeader);
-
-			}else{
-				writer = new FileWriter(csvLocation, true);
-			}
 
 			try {
-
+				List colsMapHeader = new ArrayList();
+				int rowCount = 0;
+				
 				String driverClass = properties
 						.getProperty("app.jdbc.driverClassName");
 				String url = properties.getProperty("app.jdbc.url");
 				String username = properties.getProperty("app.jdbc.username");
 				String password = properties.getProperty("app.jdbc.password");
 				String query = properties.getProperty("app.query");
-				String sessionQuery=properties.getProperty("app.query.init.session");
+				String reportLocation = properties.getProperty("report.path");
+				
+				
+				String sessionQuery = properties
+						.getProperty("app.query.init.session");
 				System.out.println("driverClass :- " + driverClass);
 				System.out.println("url :- " + url);
 				System.out.println("username :- " + username);
 				System.out.println("password :- " + password);
-				System.out.println("QUERY :- " + query);
-				System.out.println("Session init query :- " + sessionQuery);
+				//System.out.println("QUERY :- " + query);
+				//System.out.println("Session init query :- " + sessionQuery);
+				System.out.println("Report Location :- " + reportLocation);
+				
+				System.out.println("sql.path :---------- " + properties.getProperty("app.query"));
+				System.out.println("config.path :----------- " + properties.getProperty("report.path"));
+				
 				Class.forName(driverClass);
 				// step2 create the connection object
 				Connection con = DriverManager.getConnection(url, username,
@@ -160,8 +141,9 @@ public class QueryParser {
 				Statement stmt = con.createStatement();
 				// step4 execute query
 				// rs=stmt.executeQuery(Constants.query);
-				if(sessionQuery != null && !"".equals(sessionQuery.trim())){
-					System.out.println("Session Init Query executing :---------------- ");
+				if (sessionQuery != null && !"".equals(sessionQuery.trim())) {
+					System.out
+							.println("Session Init Query executing :---------------- ");
 					stmt.executeUpdate(sessionQuery);
 				}
 				System.out.println("Query executing :---------------- ");
@@ -170,50 +152,83 @@ public class QueryParser {
 				// File
 				ResultSetMetaData rsmd = rs.getMetaData();
 
+				LinkedHashMap map = getCSVHeaderData();
+				Iterator iter = map.entrySet().iterator();
+
+				while (iter.hasNext()) {
+					Map.Entry mEntry = (Map.Entry) iter.next();
+					colsMapHeader.add(mEntry.getValue());
+				}
+
+				String sheetName = "MT559_110_Report";// name of sheet
+
+				HSSFWorkbook wb = new HSSFWorkbook();
+				HSSFSheet sheet = wb.createSheet(sheetName);
+				HSSFSheet sheet1 = wb.createSheet("SQL");
+				Iterator hiter = colsMapHeader.iterator();
+				int coulmn = 0;
+				int rowno = 0;
+				
+				HSSFRow row = sheet.createRow(rowno++);
+				while (hiter.hasNext()) {
+					HSSFCell cell = row.createCell(coulmn++);
+					cell.setCellValue((String) hiter.next());
+				}
+				
+				HSSFRow rowSql = sheet1.createRow(0);
+				HSSFCell cellSql = rowSql.createCell(0);
+				cellSql.setCellValue(query);
 				int columnsNumber = rsmd.getColumnCount();
 				while (rs.next()) {
-					csvData = new HashMap();
-					for(int i=1;i<= columnsNumber;i++){
-						if(rs.getObject(i) != null){
-							csvData.put(i-1+"", rs.getObject(i).toString());
-						}else{
-							csvData.put(i-1+"", "No Value");
+					csvData = new LinkedHashMap();
+					for (int i = 1; i <= columnsNumber; i++) {
+						if (rs.getObject(i) != null) {
+							csvData.put(i - 1 + "", rs.getObject(i).toString());
+						} else {
+							csvData.put(i - 1 + "", null);
 						}
 					}
 					List colsMapData = new ArrayList();
-					
-					
-				Iterator iter1 = csvData.entrySet().iterator();
 
-				while (iter1.hasNext()) {
-					Map.Entry mEntry = (Map.Entry) iter1.next();
-					colsMapData.add(mEntry.getValue());
-					System.out.println(mEntry.getKey() + " : " +mEntry.getValue() );
-				}
-								
-			
-					CSVUtils.writeLine(writer, colsMapData);
+					Iterator iter1 = csvData.entrySet().iterator();
+					while (iter1.hasNext()) {
+						Map.Entry mEntry = (Map.Entry) iter1.next();
+						colsMapData.add(mEntry.getValue());
+					}
+					row = sheet.createRow(rowno++);
+					Iterator diter = colsMapData.iterator();
+					coulmn = 0;
+					while (diter.hasNext()) {
+						HSSFCell cell = row.createCell(coulmn);
+						cell.setCellValue((String) diter.next());
+						coulmn=coulmn+1;
+					}
 					Thread.sleep(50);
 					System.out.println("Data added successfully !!!");
-					
+
 				}
-				writer.flush();
-				writer.close();
+				String rlocation=reportLocation+fileName;
+				File directory = new File(String.valueOf(rlocation));
+				if(!directory.exists()){
+					directory.getParentFile().mkdirs();
+				 }           
+				FileOutputStream fileOut = new FileOutputStream(rlocation);
+
+				// write this workbook to an Outputstream.
+				wb.write(fileOut);
+				fileOut.flush();
+				fileOut.close();
 				con.close();
 
 			} catch (Exception e) {
-				writer.flush();
-				writer.close();
 				e.printStackTrace();// TODO: handle exception
 			}
-			
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 
 		}
-		
 
 	}
 
---
+++ src/main/java/com/parse/ReadWriteExcelFile.java
@@ -0,0 +1,209 @@
+package com.parse;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ReadWriteExcelFile {
+
+	public static void readXLSFile() throws IOException
+	{
+		InputStream ExcelFileToRead = new FileInputStream("D:\\Test.xls");
+		HSSFWorkbook wb = new HSSFWorkbook(ExcelFileToRead);
+
+		HSSFSheet sheet=wb.getSheetAt(0);
+		HSSFRow row; 
+		HSSFCell cell;
+
+		Iterator rows = sheet.rowIterator();
+
+		while (rows.hasNext())
+		{
+			row=(HSSFRow) rows.next();
+			Iterator cells = row.cellIterator();
+			
+			while (cells.hasNext())
+			{
+				cell=(HSSFCell) cells.next();
+		
+				if (cell.getCellType() == HSSFCell.CELL_TYPE_STRING)
+				{
+					System.out.print(cell.getStringCellValue()+" ");
+				}
+				else if(cell.getCellType() == HSSFCell.CELL_TYPE_NUMERIC)
+				{
+					System.out.print(cell.getNumericCellValue()+" ");
+				}
+				else
+				{
+					//U Can Handel Boolean, Formula, Errors
+				}
+			}
+			System.out.println();
+		}
+	
+	}
+	
+public static void writeXLSFile(List colsMapHeader,String header,String data,int rowno,String fileLocation) throws IOException {
+		
+		//String excelFileName = "D:\\Test.xls";//name of excel file
+
+		String sheetName = "MT559_110_Report";//name of sheet
+
+		HSSFWorkbook wb = new HSSFWorkbook();
+		HSSFSheet sheet = wb.createSheet(sheetName) ;
+		HSSFSheet sheet1 = wb.createSheet("SQL") ;
+		Iterator iter = colsMapHeader.iterator();
+		int coulmn=0;
+		HSSFRow row = sheet.createRow(0);
+		while (iter.hasNext()) {
+			
+			HSSFCell cell = row.createCell(coulmn++);
+			cell.setCellValue((String)iter.next());
+		}
+		
+		FileOutputStream fileOut = new FileOutputStream(fileLocation);
+		
+		//write this workbook to an Outputstream.
+		wb.write(fileOut);
+		fileOut.flush();
+		fileOut.close();
+	}
+	
+	public static void writeXLSFile() throws IOException {
+		
+		String excelFileName = "D:\\Test.xls";//name of excel file
+
+		String sheetName = "Deepak";//name of sheet
+
+		HSSFWorkbook wb = new HSSFWorkbook();
+		HSSFSheet sheet = wb.createSheet(sheetName) ;
+		HSSFSheet sheet1 = wb.createSheet("Abdul sir") ;
+
+		//iterating r number of rows
+		for (int r=0;r < 5; r++ )
+		{
+			HSSFRow row = sheet.createRow(r);
+	
+			//iterating c number of columns
+			for (int c=0;c < 5; c++ )
+			{
+				HSSFCell cell = row.createCell(c);
+				
+				cell.setCellValue("Cell "+r+" "+c);
+			}
+		}
+		
+		for (int r=0;r < 5; r++ )
+		{
+			HSSFRow row = sheet1.createRow(r);
+	
+			//iterating c number of columns
+			for (int c=0;c < 5; c++ )
+			{
+				HSSFCell cell = row.createCell(c);
+				
+				cell.setCellValue("Cell "+r+" "+c);
+			}
+		}
+		
+		FileOutputStream fileOut = new FileOutputStream(excelFileName);
+		
+		//write this workbook to an Outputstream.
+		wb.write(fileOut);
+		fileOut.flush();
+		fileOut.close();
+	}
+	
+	public static void readXLSXFile() throws IOException
+	{
+		InputStream ExcelFileToRead = new FileInputStream("D:\\Test.xlsx");
+		XSSFWorkbook  wb = new XSSFWorkbook(ExcelFileToRead);
+		
+		XSSFWorkbook test = new XSSFWorkbook(); 
+		
+		XSSFSheet sheet = wb.getSheetAt(0);
+		XSSFRow row; 
+		XSSFCell cell;
+
+		Iterator rows = sheet.rowIterator();
+
+		while (rows.hasNext())
+		{
+			row=(XSSFRow) rows.next();
+			Iterator cells = row.cellIterator();
+			while (cells.hasNext())
+			{
+				cell=(XSSFCell) cells.next();
+		
+				if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING)
+				{
+					System.out.print(cell.getStringCellValue()+" ");
+				}
+				else if(cell.getCellType() == XSSFCell.CELL_TYPE_NUMERIC)
+				{
+					System.out.print(cell.getNumericCellValue()+" ");
+				}
+				else
+				{
+					//U Can Handel Boolean, Formula, Errors
+				}
+			}
+			System.out.println();
+		}
+	
+	}
+	
+	public static void writeXLSXFile() throws IOException {
+		
+		String excelFileName = "D:\\Test.xlsx";//name of excel file
+
+		String sheetName = "Sheet1";//name of sheet
+
+		XSSFWorkbook wb = new XSSFWorkbook();
+		XSSFSheet sheet = wb.createSheet(sheetName) ;
+
+		//iterating r number of rows
+		for (int r=0;r < 5; r++ )
+		{
+			XSSFRow row = sheet.createRow(r);
+
+			//iterating c number of columns
+			for (int c=0;c < 5; c++ )
+			{
+				XSSFCell cell = row.createCell(c);
+	
+				cell.setCellValue("Cell "+r+" "+c);
+			}
+		}
+
+		FileOutputStream fileOut = new FileOutputStream(excelFileName);
+
+		//write this workbook to an Outputstream.
+		wb.write(fileOut);
+		fileOut.flush();
+		fileOut.close();
+	}
+
+	public static void main(String[] args) throws IOException {
+		
+		writeXLSFile();
+		/*readXLSFile();
+		
+		writeXLSXFile();
+		readXLSXFile();*/
+
+	}
+
+}
